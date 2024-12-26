4f45b66be8231cc0b1bf9489b2eaf17bf7faeb56
HalbFettKaese <43047038+HalbFettKaese@users.noreply.github.com>
2024-12-26 12:34:35 +0100

    Fixed problems with some hardware
    sqrt(int) as well as \ linebreaks weren't allowed on all machines

--- resourcepack/assets/shader_selector/shaders/include/marker_settings.glsl
+++ resourcepack/assets/shader_selector/shaders/include/marker_settings.glsl
@@ -8,13 +8,10 @@ signature:
  ADD_MARKER(channel, green, alpha, op, rate)
 */
 // append different marker definitions
-#define LIST_MARKERS \
-ADD_MARKER(EXAMPLE_GREYSCALE_CHANNEL, 253, 251, 1, 0.1) \
-ADD_MARKER(EXAMPLE_ROTATION_CHANNEL, 251, 251, 0, 0.0) \
-ADD_MARKER(EXAMPLE_ROTATION_CHANNEL, 252, 251, 4, 0.4)
+#define LIST_MARKERS ADD_MARKER(EXAMPLE_GREYSCALE_CHANNEL, 253, 251, 1, 0.1) ADD_MARKER(EXAMPLE_ROTATION_CHANNEL, 251, 251, 0, 0.0) ADD_MARKER(EXAMPLE_ROTATION_CHANNEL, 252, 251, 4, 0.4)
 
 #define MARKER_RED 254
 
 // Screen pixel that the marker ends up on if it uses channel k:
 // Mapping follows structure that is like an inverted cantor pairing (but only producing coordinates with an even sum)
-#define MARKER_POS(k) (ivec2(2*int(ceil(sqrt(k)) - 1.0),0) + (k - int((ceil(sqrt(k)) - 1.0)*(ceil(sqrt(k)) - 1.0)) - 1)*ivec2(-1, 1))
+#define MARKER_POS(k) (ivec2(2*int(ceil(sqrt(float(k))) - 1.0),0) + (k - int((ceil(sqrt(float(k))) - 1.0)*(ceil(sqrt(float(k))) - 1.0)) - 1)*ivec2(-1, 1))
